ddfede0c7bd0c11efb1f90a838666c8141e00051
Chetan Bhati <cbhati@taskme.biz>
2024-02-28 19:08:24 +0530

    pause program button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ import MenuBar from '../assets/images/menu-bar.svg';
 const Header = () => {
     useEffect(() => {
         const init = async () => {
-            const { Collapse, Dropdown, Offcanvas, initTE } = await import("tw-elements");
-            initTE({ Collapse, Dropdown, Offcanvas });
+            const { Collapse, Dropdown, Offcanvas, Tooltip, initTE } = await import("tw-elements");
+            initTE({ Collapse, Dropdown, Offcanvas, Tooltip });
         };
         init();
     }, []);
@@ -148,6 +148,14 @@ const Header = () => {
         </nav>
       </div>  
     </header>
+
+    {/* Pause Program Start */}
+    <button type="button" data-te-ripple-init data-te-ripple-color="light" className="!fixed bottom-5 right-5 rounded-full bg-light-green p-3 hover:bg-lime-600 focus:bg-lime-600 focus:outline-none focus:ring-0 z-[999]" data-te-toggle="tooltip" title="Pause Program">
+        <svg className="w-[30px] h-[30px] fill-white" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"></path>
+            </svg>
+    </button>
+     {/* Pause Program End */}
     </>
   );
 }
